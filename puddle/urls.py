fbc951f4848394cdc86cbd4b44400095923cf750
ttwange <ttwange2@gmail.com>
2023-07-20 22:47:25 +0300

    first app

--- puddle/urls.py
+++ puddle/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from core.views import index
+from django.urls import path
+from core.views import index, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('contact/',contact,name="contact")
 ]
