116fccdb3ca18dec612259aa960c680800a77c9b
ttwange <ttwange2@gmail.com>
2023-07-21 00:03:06 +0300

    items database setups

--- item/models.py
+++ item/models.py
@@ -5,6 +5,7 @@ class Category(models.Model):
     name = models.CharField(max_length=250)
 
     class Meta:
+        ordering = ('name',)
         verbose_name_plural = 'Categories'
 
     def __str__(self):
