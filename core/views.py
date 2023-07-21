27f94d6c790d8ccbd4af477ca916971544fe4256
ttwange <ttwange2@gmail.com>
2023-07-21 09:36:52 +0300

    Importing the databases so as to show the products to the front end

--- core/views.py
+++ core/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from item.models import Category,Items
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    items = Items.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+    context = {'items':items, 'categories':categories}
+    return render(request, 'core/index.html', context)
 
 def contact(request):
     return render(request, 'core/contact.html')
