ba060f8749ad5be49617bb618fa5591c2a38ac9b
ttwange <ttwange2@gmail.com>
2023-07-22 21:12:58 +0300

    Creating the item detail page

--- item/views.py
+++ item/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Items
 
 # Create your views here.
 def detail(request, pk):
-    
+    item = get_object_or_404(item, pk=pk)
