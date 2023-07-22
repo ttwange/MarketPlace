f5a05aa93c07434f3608b69eb6efba5f025e8427
ttwange <ttwange2@gmail.com>
2023-07-22 22:26:10 +0300

    added the item urls path to the main urls file path

--- puddle/urls.py
+++ puddle/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from core.views import index, contact
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('items/', include('item.urls')),
     path('', index, name='index'),
     path('contact/',contact,name="contact")
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
