0717c8d25ad3496236054eedbcc28803fd560755
ttwange <ttwange2@gmail.com>
2023-07-23 11:34:23 +0300

    validating if form submitted is valid

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from item.models import Category,Items
 from .forms import SignupForm
 
@@ -13,6 +13,12 @@ def contact(request):
     return render(request, 'core/contact.html')
 
 def signup(request):
-    form =  SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST or None )
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form =  SignupForm()
     context = {'form':form}
     return render(request, 'core/signup.html', context)
