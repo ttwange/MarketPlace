bb5abf4dd051c6bacb3c77578cde85a7d8ac6e6c
ttwange <ttwange2@gmail.com>
2023-07-23 21:48:51 +0300

    Working on the edit option of the website

--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Items
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 # Create your views here.
 def detail(request, pk):
@@ -12,17 +12,16 @@ def detail(request, pk):
     return render(request, 'item/detail.html',context)
 
 @login_required
-def new(request):
+def edit(request):
+    item = get_object_or_404(Items, pk=pk, created_by=request.user)
     if request.method == 'POST':
-        form = NewItemForm(request.POST, request.FILES)
+        form = EditItemForm(request.POST, request.FILES)
         if form.is_valid():
-            item = form.save(commit=False)
-            item.created_by = request.user
-            item.save()
+            form.save()
             return redirect('item:detail', pk=item.id)
     else:
-        form = NewItemForm()
-    context = {'form':form, 'title': 'New item'}
+        form = EditItemForm()
+    context = {'form':form, 'title': 'Edit item'}
     return render(request, 'item/form.html', context)
 
 @login_required
