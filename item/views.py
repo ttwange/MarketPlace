89b4e4a3bfdf35b2bd4ff3ee00494c50a218f2a6
ttwange <ttwange2@gmail.com>
2023-07-23 18:57:39 +0300

    Styling the Newitem form

--- item/views.py
+++ item/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Items
 from .forms import NewItemForm
@@ -13,6 +13,14 @@ def detail(request, pk):
 
 @login_required
 def new(request):
-    form = NewItemForm()
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemForm()
     context = {'form':form, 'title': 'New item'}
     return render(request, 'item/form.html', context)
