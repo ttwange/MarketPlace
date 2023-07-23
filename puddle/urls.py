a24c0d37778dcf628e2eea07f0dd98643778461b
ttwange <ttwange2@gmail.com>
2023-07-23 19:21:22 +0300

    Working on the dashboard urls path

--- puddle/urls.py
+++ puddle/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('core.urls')),
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('items/', include('item.urls')),
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
