4f88f422ea3a76509423b86afed862e5563f7ef4
ttwange <ttwange2@gmail.com>
2023-07-23 19:08:49 +0300

    Working on the dashboard views

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from item.models import Items
 
 # Create your views here.
+@login_required
+def index(request):
+    item = Items.objects.filer
