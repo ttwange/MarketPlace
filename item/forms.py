ecb237ccb11857fd24500b5f78c4361e44824ed0
ttwange <ttwange2@gmail.com>
2023-07-23 18:39:35 +0300

    Styling the Newitem form

--- item/forms.py
+++ item/forms.py
@@ -3,14 +3,26 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Items
 
 
+
+INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Items
         fields = ('category', 'name','description','price','image')
-
-
         widgets = {
             'category': forms.Select(attrs={
-                'class':'w-full py-4 px-6 rounded-xl border'
+                'class': INPUT_CLASSES
+            }),
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'price': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
             })
         }
