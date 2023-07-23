42fe00f229e5c57170d0528d08c1cec58cea86a1
ttwange <ttwange2@gmail.com>
2023-07-23 10:09:58 +0300

    Creating the url patterns for the sign up page

--- puddle/urls.py
+++ puddle/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
-from core.views import index, contact
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
