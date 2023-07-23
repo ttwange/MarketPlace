5d971a3c292827938c9119ffe5c526e6ca1276d0
ttwange <ttwange2@gmail.com>
2023-07-23 17:19:03 +0300

    creating the login model in the forms file

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,14 @@
 from django import forms 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder':'Username', 'class':'w-full py-4 px-6 rounded-xl'
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Confrim Password', 'class':'w-full py-4 px-6 rounded-xl'
+    }))
 
 class SignupForm(UserCreationForm):
     class Meta:
