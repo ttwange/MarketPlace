f17ebf03412faf53b1bb18e7dccd41d673402ce5
ttwange <ttwange2@gmail.com>
2023-07-24 09:50:26 +0300

    Working on the searching functionality for the website

--- item/views.py
+++ item/views.py
@@ -7,12 +7,14 @@ from .forms import NewItemForm,EditItemForm
 # Create your views here.
 def items(request):
     query = request.GET.get('query','')
-    category_id = request.GET.get('category_id',0)
+    category_id = request.GET.get('category',0)
     items = Items.objects.filter(is_sold=False)
     categories = Category.objects.all()
+    if category_id:
+        items = items.filter(category_id=category_id)
     if query:
         items = items.filter(Q(name__icontains = query)|Q(description__icontains=query))
-    context = {'items':items,'query':query,'categories':categories}
+    context = {'items':items,'query':query,'categories':categories, 'category_id':int(category_id)}
     return render(request,'item/items.html',context)
 
 
